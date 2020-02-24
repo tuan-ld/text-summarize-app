1bea604341091626e101a54bda0b73b2452c1a25
Tuan <ldt.uet@gmail.com>
2020-02-24 10:51:40 +0900

    Initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'o(y_v=vc+co9rsviy!0k2j3*b1!^=g6wk)nk=nyk-(8g+2fwc4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [herokusites.herokuapp.com]
 
 
 # Application definition

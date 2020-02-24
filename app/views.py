178720b45ddc23e10e4233f41605160dc438eb2c
Tuan <ldt.uet@gmail.com>
2020-02-24 12:57:19 +0900

    Initial commit

--- app/views.py
+++ app/views.py
@@ -12,13 +12,8 @@ def landing_view(request):
 	if form1.is_valid():
 		result = getSummary.main(form1.cleaned_data.get('Text'))
 		form1 = Paragraph()
-		# form2 = UploadFileForm()
-	# elif form2.is_valid():
-	# 	result = getSummary.main(handle_file(request.FILES['Upload']))
-	# 	form1 = Paragraph()
 	else:
 		form1 = Paragraph()
-		# form2 = UploadFileForm()
 	context = {
 		"form1" : form1,
 		# "form2" : form2,
@@ -26,12 +21,6 @@ def landing_view(request):
 	}
 	return render(request, "landing.html", context)
 
-
-# def handle_file(f):
-# 	f.seek(0)
-# 	para = f.read()
-# 	para = para.replace('\n',' ')
-# 	return para
 
 def index(request):
 	context = RequestContext(request)
